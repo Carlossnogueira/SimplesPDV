3c5765f943ec105c914306d3b4679171fb609c3d
Carlossnogueira <carlosnogueira.tkdev@gmail.com>
2025-09-25 20:21:59 -0300

    fix: Error message if Product quantity is greater than 9999 and finishing product validator tests

--- src/SimplesPDV.Application/Services/Product/Create/CreateProductValidator.cs
+++ src/SimplesPDV.Application/Services/Product/Create/CreateProductValidator.cs
@@ -9,6 +9,8 @@ public class CreateProductValidator : AbstractValidator<RequestProductJson>
     {
         RuleFor(p =>  p.Name).NotEmpty().WithMessage("Name is required");
         RuleFor(p=>p.Price).GreaterThan(0).WithMessage("Price must be greater than 0");
-        RuleFor(p=>p.Quantity).GreaterThanOrEqualTo(1).WithMessage("Quantity is required and greater than 1");
+        RuleFor(p => p.Quantity)
+            .GreaterThanOrEqualTo(1).WithMessage("Quantity must be at least 1")
+            .LessThanOrEqualTo(9999).WithMessage("Quantity must be less than or equal to 9999");
     }
 }
--- test/ValidatorsTests/Product/Create/CreateProductValidatorTests.cs
+++ test/ValidatorsTests/Product/Create/CreateProductValidatorTests.cs
@@ -32,4 +32,44 @@ public class CreateProductValidatorTests
         result.IsValid.Should().BeFalse();
         result.Errors.Should().ContainSingle().And.Contain(e => e.ErrorMessage.Equals("Name is required"));
     }
+    
+    [Theory]
+    [InlineData(0)]
+    [InlineData(-1)]
+    public void Error_Price_Less_Than_Zero(decimal price)
+    {
+        var validator = new CreateProductValidator();
+        var request = (RequestProductJson)RequestCreateProductJsonBuilder.Build();
+        request.Price = price;
+        var result = validator.Validate(request);
+
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().ContainSingle().And.Contain(e => e.ErrorMessage.Equals("Price must be greater than 0"));
+    }
+    
+    [Theory]
+    [InlineData(0)]
+    [InlineData(-1)]
+    public void Error_Quantity_Less_Than_Zero(int quantity)
+    {
+        var validator = new CreateProductValidator();
+        var request = (RequestProductJson)RequestCreateProductJsonBuilder.Build();
+        request.Quantity = quantity;
+        var result = validator.Validate(request);
+        
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().ContainSingle().And.Contain(e => e.ErrorMessage.Equals("Quantity is required and greater than 1"));
+    }
+
+    [Fact]
+    public void Error_Product_More_Than_Nine_Thousand_Nine_Hundred_ninety_nine()
+    {
+        var validator = new CreateProductValidator();
+        var request = (RequestProductJson)RequestCreateProductJsonBuilder.Build();
+        request.Quantity = 99999;
+        var result = validator.Validate(request);
+        
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().ContainSingle().And.Contain(e => e.ErrorMessage.Equals("Quantity must be less than or equal to 9999"));
+    }
 }
